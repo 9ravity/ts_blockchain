423f83786a8df002594cc0484e3f7d6df2860dd4
9ravity <shegoback@naver.com>
2020-02-26 21:47:03 +0900

    blockChain-method create

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
@@ -17,12 +19,38 @@ class Block {
     this.data = data;
     this.timestamp = timestamp;
   }
+  static calculateBlockHash = (
+    index: number,
+    previosHash: string,
+    timestamp: number,
+    data: string
+  ): string => {
+    return CryptoJS.SHA256(index + previosHash + timestamp + data).toString();
+  };
 }
 
 const geneisBlock: Block = new Block(0, "fgafsd", "", "ahahah", 21414);
 
-let blockchain: [Block] = [geneisBlock];
+let blockchain: Block[] = [geneisBlock];
 
 console.log("blockchain : " + JSON.stringify(blockchain));
+
+const getBlockchain = (): Block[] => {
+  return blockchain;
+};
+
+const getLatestBlock = (): Block => {
+  return blockchain[blockchain.length - 1];
+};
+const getNewTimeStamp = (): number => {
+  return Math.round(new Date().getTime() / 1000);
+};
+
+const createNewBlock = (data: string): Block => {
+  const previosBlock: Block = getLatestBlock();
+  const newIndex: number = previosBlock.index + 1;
+  const nextTimestamp: number = getLatestBlock().timestamp;
+  return;
+};
 
 export {};
